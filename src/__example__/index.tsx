d1ffe9ca9a70909b744a7f7afa2658e97f5a7e2f
Leonid Trofymchuk <stesel23@gmail.com>
2019-06-26 22:16:17 +0300

    RWF-00 Add example page

---
+++ src/__example__/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const App: React.FC = () => {
+    return <h1>React With Force!</h1>;
+};
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("app"),
+);
